3356eeb66f2c6d29fcb38645419e1dde154a66ae
jmj <jmj@kookmin.ac.kr>
2023-11-12 12:40:23 +0900

    mongo db connected

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const mongoose = require('mongoose')
+const Campground = require('./models/campground')
 
-const port = 3000
-
+main().catch(err => console.log(err));
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+    console.log('Database connected')
+    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
@@ -11,6 +17,13 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({title: 'My Backyard', description: 'cheap camping'})
+    await camp.save()
+    res.send(camp)
+})
+
+const port = 3000
 app.listen(port, () => {
     console.log(`Serving on port ${port}`)
 })
