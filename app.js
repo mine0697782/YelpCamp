c48bcc436900be5c08ee7f1726fbb52ca8b7107a
Poppy <jmj@kookmin.ac.kr>
2024-04-18 15:38:36 +0900

    npm i 'Joi' / add schema valdating / add error view

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
 const ejsMate = require('ejs-mate')
+const { campgroundSchema } = require('./schemas.js')
 const catchAsync = require('./utils/catchAsync')
+const ExpressError = require('./utils/ExpressError')
 const Campground = require('./models/campground')
+
 const methodOverride = require('method-override')
 const morgan = require('morgan')
 
@@ -24,6 +27,16 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 app.use(morgan('dev'))
+
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body)
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next()
+    }
+}
 
 const verifyPassword = (req, res, next) => {
     const { password } = req.query
@@ -54,7 +67,9 @@ app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new')
 })
 
-app.post('/campgrounds', catchAsync(async (req, res, next) => {
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
+    // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400)
+    
     const campground = new Campground(req.body.campground)
     await campground.save()
     res.redirect(`/campgrounds/${campground._id}`)
@@ -74,8 +89,8 @@ app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     res.render('campgrounds/edit', { campground })
 }))
 
-app.put('/campgrounds/:id', catchAsync(async (req, res) => {
-    const { id } = req.params
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
+const { id } = req.params
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
     // 위의 코드와 아래의 코드는 동일하게 동작함
     // const campground = await Campground.findById(id)
@@ -93,12 +108,18 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     res.redirect('/campgrounds')
 }))
 
-app.use((req, res) => {
-    res.status(404).send('NOT FOUND!')
+// app.use((req, res) => {
+//     res.status(404).send('NOT FOUND!')
+// })
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
 })
 
 app.use((err, req, res, next) => {
-    res.send('Oh, something weng wrong!')
+    const {statusCode = 500} = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
 })
 
 app.listen(3000, () => {
